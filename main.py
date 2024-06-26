657026227ecf3dff7545398ae4ab323a7c0cc0e1
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-06-26 16:55:12 -0600

    completed project

--- main.py
+++ main.py
@@ -6,3 +6,25 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+def write_letter(names):
+    with open('./Input/Letters/starting_letter.txt') as file:
+        letter = file.read()
+        
+        for name in names:
+            print(name)
+            new_letter = letter.replace('[name]', name)
+            print(new_letter)
+            with open(f'./Output/ReadyToSend/{name}.txt', 'w') as new_file:
+                new_file.write(new_letter)
+
+
+def get_names():
+    with open('./Input/Names/invited_names.txt') as names:
+        name_list = names.read().splitlines()
+        print(name_list)
+        return name_list
+    
+
+write_letter(get_names())
